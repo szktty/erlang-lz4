1daa35e052a849c6f07ca581e32abd9f9159322c
SUZUKI Tetsuya <suzuki@spice-of-life.net>
2012-10-01 00:17:24 +0900

    impl pack, unpack

--- src/lz4.erl
+++ src/lz4.erl
@@ -1,10 +1,12 @@
 -module(lz4).
 
--export([compress/1, compress/2, uncompress/2]).
+-export([compress/1, compress/2, uncompress/2,
+    pack/1, pack/2, unpack/1]).
 
 -on_load(init/0).
 
 -type option() :: high | {block, integer()}.
+-type pack() :: {binary(), integer()}.
 
 -define(nif_stub, nif_stub_error(?LINE)).
 nif_stub_error(Line) ->
@@ -32,4 +34,21 @@ compress(_Binary, _Options) ->
 -spec uncompress(binary(), integer()) -> {ok, binary()} | {error, term()}.
 uncompress(_Binary, _OnigSize) ->
     ?nif_stub.
+
+-spec pack(binary()) -> {ok, binary()} | {error, term()}.
+pack(Binary) ->
+    pack(Binary, []).
+
+-spec pack(binary(), [option()]) -> {ok, pack()} | {error, term()}.
+pack(Binary, Options) ->
+    case compress(Binary, Options) of
+        {ok, Compressed} ->
+            {ok, {Compressed, byte_size(Binary)}};
+        Error ->
+            Error
+    end.
+
+-spec unpack(pack()) -> {ok, binary()} | {error, term()}.
+unpack({Binary, OrigSize}) ->
+    uncompress(Binary, OrigSize).
 
